3ab1ed480bf4c7693b0497e3bbba79bd727b55e4
David Boedigheimer <stealth259@gmail.com>
2019-09-03 15:50:35 -0700

    Add click functions to half of the flashcards.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,77 @@
 $(document).ready(function() {
-  
+  $("#java").click(function() {
+    $("#javaHide").toggle();
+    $("#javaShow").toggle();
+  });
+
+  $("#operator").click(function() {
+    $("#operHide").toggle();
+    $("#operShow").toggle();
+  });
+
+  $("#variable").click(function() {
+    $("#varHide").toggle();
+    $("#varShow").toggle();
+  });
+
+  $("#VNC").click(function() {
+    $("#vncHide").toggle();
+    $("#vncShow").toggle();
+  });
+
+  $("#function").click(function() {
+    $("#funHide").toggle();
+    $("#funShow").toggle();
+  });
+
+
+  $("#method").click(function() {
+    $("#methHide").toggle();
+    $("#methShow").toggle();
+  });
+
+  $("#argument").click(function() {
+    $("#argHide").toggle();
+    $("#argShow").toggle();
+  });
+
+  $("#parameter").click(function() {
+    $("#paraHide").toggle();
+    $("#paraShow").toggle();
+  });
+
+  $("#return").click(function() {
+    $("#retHide").toggle();
+    $("#retShow").toggle();
+  });
+
+  $("#CM").click(function() {
+    $("#chainHide").toggle();
+    $("#chainShow").toggle();
+  });
+
+  $("#java").click(function() {
+    $("#").toggle();
+    $("#").toggle();
+  });
+
+  $("#java").click(function() {
+    $("#").toggle();
+    $("#").toggle();
+  });
+
+  $("#java").click(function() {
+    $("#").toggle();
+    $("#").toggle();
+  });
+
+  $("#java").click(function() {
+    $("#").toggle();
+    $("#").toggle();
+  });
+
+  $("#java").click(function() {
+    $("#").toggle();
+    $("#").toggle();
+  });
 });
